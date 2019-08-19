14c096950237a56a0a4e106f5255e201d0a6da31
Imran Sayyed <isayyed@equalexperts.com>
2019-08-19 14:54:26 +0530

    Fix error in aws json

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,7 +9,7 @@ function App() {
     <Router>
       <div className="App">
         <header className="App-header">
-          <h1 className="App-title">Welcome to react</h1>
+          <h1 className="App-title">Fib calculator</h1>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
         </header>
